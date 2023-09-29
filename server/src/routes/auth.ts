619d203eed90ede6756dd170d5d51eb05722fac2
Tushar Ranjan Nayak <tusharnayak1515@gmail.com>
2023-09-29 18:47:15 +0530

    bug fix 16

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -38,7 +38,7 @@ router.get(
                 return res.redirect('/api/auth/login/failed');
             }
 
-            const token = authInfo?.token;
+            const token = `Bearer ${authInfo?.token}`;
             const user = authInfo?.user;
 
             res.cookie("authorization", token, {
